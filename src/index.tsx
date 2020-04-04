638c9f8ae504e54d5cc806a77f5d7449b2e7c355
Josep M Sobrepere <josepot@gmail.com>
2020-04-04 17:10:28 +0200

    Remove MyComponent fron index :')

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 
 type NotFunction<T> = T extends Function ? never : T;
 
@@ -24,27 +24,3 @@ export function useRecord<K extends keyof any, V>(
     return [state, setEntry, setState] as const;
   }, []);
 }
-
-const initialState: Record<string, number> = { foo: 0, bar: 0, baz: 0 };
-const inc = (x: number) => x + 1;
-const dec = (x: number) => x - 1;
-
-export const MyComponent: React.FC = () => {
-  const [state, setRecord, setRecords] = useRecord(initialState);
-
-  return (
-    <>
-      <ul>
-        {Object.entries(state).map(([key, val]) => (
-          <li key={key}>
-            <button onClick={() => setRecord(key, dec)}>-</button>
-            {key}: {val}
-            <button onClick={() => setRecord(key, inc)}>+</button>
-            <button onClick={() => setRecord(key, 0)}>RESET</button>
-          </li>
-        ))}
-      </ul>
-      <button onClick={() => setRecords(initialState)}>RESET ALL</button>
-    </>
-  );
-};
